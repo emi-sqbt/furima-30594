76a1777c998c903faea293645d5a263629b9c2f8
emi-sqbt <71768074+emi-sqbt@users.noreply.github.com>
2020-10-27 21:32:25 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -15,7 +15,12 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
-    end
+    end 
+
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
