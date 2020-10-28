753fa6dc6b4fc29d7ce13e36bc605c1e86a409b4
emi-sqbt <71768074+emi-sqbt@users.noreply.github.com>
2020-10-28 15:51:56 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+  
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
 
   private
 
