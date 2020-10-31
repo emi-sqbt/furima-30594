b86868472d63e6d7567c63ec57bff425176e83c7
emi-sqbt <71768074+emi-sqbt@users.noreply.github.com>
2020-10-31 21:49:28 +0900

    sold outを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,42 +1,54 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :sold_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
-    @item = Item.find(params[:item_id])
-    
-    
   end
   
 
 
   def create
-    @item = Item.find(params[:item_id])
+   
     @order_form = OrderForm.new(order_params)
-    if @order_form.valid?
-      pay_item
+     if @order_form.valid?
+       pay_item
        @order_form.save
        redirect_to root_path
-    else
+     else
        render action: :index
-    end
+     end
+    
 
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+  def sold_item
+    if @item.order != nil
+      redirect_to root_path
+    end
+  end
 
   def order_params
     params.require(:order_form).permit(:prefecture_id, :postal_number, :city, :house_number, :building_number, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
-     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]   # 自身のPAY.JPテスト秘密鍵を記述しましょう
+     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]   
      Payjp::Charge.create(
-     amount: Item.find(params[:item_id]).price,  # 商品の値段
-     card: order_params[:token],    # カードトークン
-     currency: 'jpy'                 # 通貨の種類（日本円）
+     amount: Item.find(params[:item_id]).price,  
+     card: order_params[:token],   
+     currency: 'jpy'               
     )
   end
 
+  
 
 end
 
